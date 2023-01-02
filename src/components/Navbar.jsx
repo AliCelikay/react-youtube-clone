bcd808e95c609c1df1c8dc93c0d27ba8a789fd76
AliCelikay <alicelikay129@gmail.com>
2023-01-02 11:34:57 -0600

    Created File and folder structure

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// used for 1 dimensional layouts column or row
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar;
