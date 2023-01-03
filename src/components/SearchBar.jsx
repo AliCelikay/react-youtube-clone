6f772a94682cbc5bb50d5e5994adb801788c75aa
AliCelikay <alicelikay129@gmail.com>
2023-01-03 13:13:21 -0600

    searc feed working

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,36 +1,51 @@
-import React,{useState} from 'react';
-import {useNavigate} from 'react-router-dom';
-import {Paper, IconButton} from '@mui/material';
-import { Search} from '@mui/icons-material';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Paper, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 const SearchBar = () => {
-  return (
-    // Paper is a div w/ white background and elevation from mui, floating like
-    <Paper
-        component="form"
-        onSubmit={()=>{}}
-        sx={{
-            borderRadius: 20,
-            border: '1px solid #e3e3e3', 
-            pl: 2,
-            boxShadow: 'none',
-            mr: { sm: 5}
-        }}
-    >
-        <input
-            className="search-bar"
-            placeholder="Search..."
-            value=""
-            onChange={()=>{}}
-        />
-        <IconButton
-            type="submit"
-            sx={{p:'10px', color:'red'}}
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+    
+    // function for placing searched term in params
+    const onHandleSubmit = (e) => {
+        e.preventDefault();
+
+        if(searchTerm){
+            // use navigate hook provided to navigate to this specific url
+            navigate(`/search/${searchTerm}`);
+        }
+        // reseting the url after navigating
+        setSearchTerm('');
+    }
+
+    return (
+        // Paper is a div w/ white background and elevation from mui, floating like
+        <Paper
+            component="form"
+            onSubmit={onHandleSubmit}
+            sx={{
+                borderRadius: 20,
+                border: '1px solid #e3e3e3',
+                pl: 2,
+                boxShadow: 'none',
+                mr: { sm: 5 }
+            }}
         >
-            <Search/>
-        </IconButton>
-    </Paper>
-  )
+            <input
+                className="search-bar"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <IconButton
+                type="submit"
+                sx={{ p: '10px', color: 'red' }}
+            >
+                <SearchIcon />
+            </IconButton>
+        </Paper>
+    )
 }
 
 export default SearchBar
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { Box, Typography } from '@mui/material'
+import {useParams} from 'react-router-dom';
+
+// import Videos from './Videos'
+import { Videos } from './'; // ./components
+
+import { fetchFromAPI } from '../utils/fetchFromApi';
+
 
 const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  // reading searchterm in params implemented by the navigate hook in SearchBar.jsx
+  const { searchTerm } = useParams();
+
+  // to load the page of content search by navbar
+  useEffect(() => {
+    // using the searchTerm in params implemented by navigate hook in SearchBar.jsx
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items))
+
+    // searchTerm as the dependency array so code will run only when a new search 
+  }, [searchTerm]);
+
+
   return (
-    <div>SearchFeed</div>
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+        Search Results for: <span style={{ color: '#FC1503' }}>{searchTerm}</span>Videos
+      </Typography>
+      <Box display="flex">
+        <Box sx={{ mr: { sm: '100px' } }}/>
+        {<Videos videos={videos} />}
+      </Box>
+    </Box>
   )
 }
 
